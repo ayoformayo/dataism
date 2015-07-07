9adbfdb6cc03b5e54bdd292e617fe61d83e6bacc
Jimmi Carney <jcarney@urbanbound.com>
2015-07-06 23:16:44 -0500

    gulp gets css too

--- gulpfile.js
+++ gulpfile.js
@@ -135,7 +135,7 @@ gulp.task('buildBundle', ['styles', 'buildScripts', 'bower'], function() {
 
 // Bower helper
 gulp.task('bower', function() {
-    gulp.src('app/bower_components/**/*.js', {
+    gulp.src(['app/bower_components/**/*.js', 'app/bower_components/**/*.css'], {
             base: 'app/bower_components'
         })
         .pipe(gulp.dest('public/bower_components/'));
