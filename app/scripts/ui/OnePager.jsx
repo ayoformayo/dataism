ec26aef223279d8964ddf396798e0a2b2098f8f1
Jimmi Carney <jcarney@urbanbound.com>
2015-07-14 13:20:22 -0400

    nice scroll action!

--- app/scripts/ui/OnePager.jsx
+++ app/scripts/ui/OnePager.jsx
@@ -2,12 +2,12 @@ const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 var _ = require('underscore');
 var PAGES = [
   {
-    name: 'liquor_licenses',
+    name: 'liquorLicenses',
     anchor: '#liquor_licenses',
     component: require('./liquor_maps.jsx')
   },
   {
-    name: 'heat_map',
+    name: 'heatMap',
     anchor: '#heat_map',
     component: require('./UBHeatMap.jsx')
   },
@@ -22,91 +22,32 @@ var Router = require('react-router');
 var OnePager = React.createClass({
   mixins: [ Router.Navigation, Router.State ],
 
-  getInitialState() {
-    var slug = this.getParams().slug;
-    return { slug: slug, page: 0 };
-  },
-
-  moveDown(){
-    if(this.state.page >= PAGES.length -1) return true;
-    var pageIndex = this.state.page;
-    var $this = $(this.getDOMNode());
-    pageIndex++
-    this.setState({page: pageIndex}, () =>{
-      var index = this.state.page >= PAGES.length ? PAGES.length -1 : this.state.page;
-
-      var anchor = PAGES[index].anchor;
-      var top = $this.find(anchor).offset().top;
-
-
-      $("html,body").animate(
-       {
-         scrollTop: top,
-       }, 2000, 'easeOutExpo');
-    })
-    
-  },
-
-  moveUp(){
-    if(this.state.page <= 0 ) return true;
-    var pageIndex = this.state.page;
-    var $this = $(this.getDOMNode());
-    pageIndex--
-    this.setState({page: pageIndex}, () =>{
-      var index = this.state.page >= PAGES.length ? PAGES.length -1 : this.state.page;
-
-      var anchor = PAGES[index].anchor;
-      var top = $this.find(anchor).offset().top;
-      $("html,body").animate(
-       {
-           scrollTop: top,
-       }, 2000, 'easeOutExpo');
-    })
-  },
-
   handleScroll(props){
-    if(!this.moving) {
+    if(!this.moving){
       this.moving = true;
-        if (props.deltaY > 0){
-    
-          this.moveDown();
+      var slug = this.getParams().slug || PAGES[0].name;
+      var currentIndex;
+      _.each(PAGES, (element, index, array) => {
+        if(element.name === slug){
+          currentIndex = index;
         }
-        if (props.deltaY < 0){
-    
-          this.moveUp();
-        }
-      _.delay(() => {
+      });
+      var nextIndex;
+      if (props.deltaY > 0 && currentIndex < PAGES.length - 1) nextIndex = currentIndex + 1;
+      if (props.deltaY < 0 && currentIndex > 0) nextIndex = currentIndex - 1;
+      if(_.isUndefined(nextIndex)) nextIndex = currentIndex;
+      var comp = PAGES[nextIndex];
+      var that = this;
+      if(nextIndex !== currentIndex){
+        this.transitionTo('activity-feed',   {slug: comp.name });
+      }
+     _.delay(() => {
         this.moving = false; 
       }, 2000);
-    }else {
-      props.preventDefault();
-      props.stopPropagation();
     }
   },
 
   onTouchEnd(e){
-    // e.preventDefault();
-    //   e.stopPropagation();
-    // var lastY = this.startY;
-    // if(!this.moving) {
-    //   this.moving = true;
-    //   if(lastY){
-    //     if(e.changedTouches[0].clientY < lastY){
-    //       this.moveDown();
-    //     }else{
-    //       this.moveUp()
-    //     }
-    //   }
-
-    //   this.lastY = e.changedTouches[0].clientY;
-
-    //   _.delay(() => {
-    //     this.moving = false; 
-    //   }, 2000);
-    // }else {
-    // }
-      // e.preventDefault();
-      // e.stopPropagation();
   },
 
   onTouchStart(e){
@@ -126,13 +67,26 @@ var OnePager = React.createClass({
         </section>
       )
     });
-
+    var pageScoller = {
+      height: sectionHeight,
+      width: sectionWidth
+    };
+    var slug = this.getParams().slug || PAGES[0].name;
+    var ref = this.refs[slug];
+    var slug = this.getParams().slug || PAGES[0].name;
+    var currentIndex;
+    _.each(PAGES, (element, index, array) => {
+      if(element.name === slug){
+        currentIndex = index;
+      };
+    });
+    pageScoller.bottom =  currentIndex * sectionHeight;
     return(
       <div className='pages-container' style={{height: sectionHeight, width: sectionWidth, position: "relative"}} onWheel={this.handleScroll} onTouchStart={this.onTouchStart} onTouchEnd={this.onTouchEnd}> 
-        <ReactCSSTransitionGroup transitionName="page" transitionAppear={true}>
-          {pages}
-        </ReactCSSTransitionGroup>
-      </div>
+       <div className='pages-scroller' style={pageScoller}> 
+         {pages}
+       </div>
+     </div>
     )
   }
 });
