43626fadcf7c1568f6af8a327430f4f990733c3f
William Carney <Jimmi@Williams-MacBook-Pro.local>
2015-09-10 17:51:58 -0400

    working transitions between pages

--- app/scripts/ui/OnePager.jsx
+++ app/scripts/ui/OnePager.jsx
@@ -1,37 +1,40 @@
 const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+const Glyphicon = require('react-bootstrap').Glyphicon;
 var _ = require('underscore');
 var PAGES = [
-  // {
-  //   name: 'liquorLicenses',
-  //   anchor: '#liquor_licenses',
-  //   component: require('./liquor_maps.jsx')
-  // },
-  // {
-  //   name: 'heatMap',
-  //   anchor: '#heat_map',
-  //   component: require('./UBHeatMap.jsx')
-  // },
-  // {
-  //   name: 'sankey',
-  //   anchor: '#sankey',
-  //   component: require('./sankey.jsx')
-  // },
-  // {
-  //   name: 'graffitiMap',
-  //   anchor: '#graffiti',
-  //   component: require('./GraffitiMap.jsx')
-  // },
-  {
-    name: 'chicagoLanguages',
-    anchor: '#languages',
-    component: require('./ChicagoLanguages.jsx')
-  }
-
-]
+  [
+    {
+      name: 'liquorLicenses',
+      anchor: '#liquor_licenses',
+      component: require('./liquor_maps.jsx')
+    },
+    {
+      name: 'chicagoLanguages',
+      anchor: '#languages',
+      component: require('./ChicagoLanguages.jsx')
+    },
+  ],
+  [
+    {
+      name: 'heatMap',
+      anchor: '#heat_map',
+      component: require('./UBHeatMap.jsx')
+    },
+    {
+      name: 'sankey',
+      anchor: '#sankey',
+      component: require('./sankey.jsx')
+    }
+  ],
+];
 var Router = require('react-router');
+var ContainerRow = require('./ContainerRow.jsx');
 
 var OnePager = React.createClass({
   mixins: [ Router.Navigation, Router.State ],
+  getInitialState(){
+    return {row: 0, col: 0, direction: 'down'};
+  },
 
   handleScroll(props){
     if (props.deltaY > 0 ) this.handleMove('up');
@@ -58,7 +61,7 @@ var OnePager = React.createClass({
         this.transitionTo('activity-feed',   {slug: comp.name });
       }
      _.delay(() => {
-        this.moving = false; 
+        this.moving = false;
       }, 2000);
     }
   },
@@ -74,41 +77,112 @@ var OnePager = React.createClass({
     this.startY = e.changedTouches[0].clientY;
   },
 
+  renderComponentRow(){
+
+  },
+
+  transitionViz(args){
+    var col = args.column;
+    var row = args.row;
+
+    switch(args.direction){
+      case 'right':
+        col++;
+        break;
+      case 'left':
+        col--;
+        break;
+      case 'up':
+        row--;
+        break;
+      case 'down':
+        row++;
+        break;
+    }
+
+    var nextComp = PAGES[row][col];
+    this.setState({direction: args.direction}, ()=>{
+      this.transitionTo('activity-feed',   {slug: nextComp.name });
+    });
+
+  },
+
+  renderComp(args){
+    var obj = PAGES[args.row][args.col];
+    var Page = obj.component;
+    return (
+      <section key={obj.name} style={{height: '100%', width: '100%', display: 'inline-block'}}>
+        <a id={obj.name} />
+        <Page ref={obj.name} />
+      </section>
+    );
+  },
+
   render(){
     var sectionHeight = window.innerHeight;
     var sectionWidth = window.innerWidth;
-    var pages = _.map(PAGES, (obj, i) => {
-      var key = 'page_' + i;
-      var Page = obj.component;
-      console.log(obj.name)
-      return (
-        <section key={key} style={{height: '100%', width: '100%', position: "relative"}}>
-          <a id={obj.name} />
-          <Page ref={obj.name} />
-        </section>
-      )
-    });
     var pageScoller = {
-      height: '100%',
-      width: '100%',
+      // height: '100%',
+      // width: '100%',
       perspective: sectionHeight +'px',
       '-moz-perspective': sectionHeight +'px',
       '-webkit-perspective': sectionHeight +'px'
     };
+    var direction = 'scroll-' + this.state.direction;
+    var pageScollerHash = { 'pages-scroller': true };
+    pageScollerHash[direction] = true;
+    var pageScollerClass = classNames(pageScollerHash);
     var slug = this.getParams().slug || PAGES[0].name;
     var ref = this.refs[slug];
-    var slug = this.getParams().slug || PAGES[0].name;
-    var currentIndex;
-    _.each(PAGES, (element, index, array) => {
-      if(element.name === slug){
-        currentIndex = index;
-      };
+    var colIndex;
+    var rowIndex;
+    if(!this.getParams().slug){
+      colIndex = 0;
+      rowIndex = 0;
+    }
+    _.each(PAGES, (row, rowI, array) => {
+      console.log(row)
+      console.log(rowIndex)
+      console.log(array)
+
+      _.each(row, (element, colI) => {
+        if(element.name === slug){
+          console.log(element.name)
+          console.log('col', colI)
+          console.log('row', rowI)
+          colIndex = colI;
+          rowIndex = rowI;
+        };
+      });
+
     });
-    pageScoller.transform = 'translateY(' + currentIndex * -sectionHeight + 'px)';
+    var arrows = [
+      {direction: 'left', name: 'chevron-left'},
+      {direction: 'right', name: 'chevron-right'},
+      {direction: 'up', name: 'chevron-up'},
+      {direction: 'down', name: 'chevron-down'}
+    ];
+    var arrowIcons = _.map(arrows, (arrow)=>{
+      var options = {
+        direction: arrow.direction,
+        column: colIndex,
+        row: rowIndex
+      }
+      return(
+        <Glyphicon glyph={arrow.name} onClick={this.transitionViz.bind(this, options)}/>
+      );
+    });
+    var page = this.renderComp({col: colIndex, row: rowIndex});
+
     return(
-      <div className='pages-container' style={{height: '100%', width: '100%', position: "fixed"}} onWheel={this.handleScroll} onTouchStart={this.onTouchStart} onTouchEnd={this.onTouchEnd}> 
-       <div className='pages-scroller' style={pageScoller}> 
-         {pages}
+      <div className='pages-container' style={{height: '100%', width: '100%', position: "fixed"}} onWheel={this.handleScroll} onTouchStart={this.onTouchStart} onTouchEnd={this.onTouchEnd}>
+       <div className={pageScollerClass} style={pageScoller}>
+         <div className='arrow-container' style={{width: '100%', height: '100%', position: 'absolute'}}>
+           {arrowIcons}
+         </div>
+         <ReactCSSTransitionGroup transitionName='viz'>
+           {page}
+         </ReactCSSTransitionGroup>
        </div>
      </div>
     )
@@ -116,3 +190,4 @@ var OnePager = React.createClass({
 });
 
 module.exports = OnePager;
+//
